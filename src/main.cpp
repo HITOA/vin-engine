1a12e322800fa513fd7d14100cdb95e734d07558
HITOA <charli.poirier@gmail.com>
2022-07-08 14:12:29 +0200

    Lua/Luaaa added

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
-#include <vin.hpp>
+#include <vincore.hpp>
 #include <error.hpp>
 #include <exception>
+#include <luaaa/luaaa.hpp>
 
 int main(int argc, char* argv[]) {
 	try {
-
+		lua_State* ls = luaL_newstate();
 	}
 	catch (const std::exception& err) {
 		Vin::HandleUnmanagedError(err, 1);
---
+++ src/vincore.cpp
@@ -0,0 +1,1 @@
+#include <vincore.hpp>
